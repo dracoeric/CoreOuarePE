bb9f77fd04394e1e664639c777d368a40e93e72c
Eric LI <erli@student.42.fr>
2019-02-13 11:02:42 +0100

    Ajout des name or comment avant fusion dans get header

--- ASM/srcs/asm_match_name_or_comment.c
+++ ASM/srcs/asm_match_name_or_comment.c
@@ -6,7 +6,7 @@
 /*   By: erli <erli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 12:19:57 by erli              #+#    #+#             */
-/*   Updated: 2019/02/12 14:20:59 by erli             ###   ########.fr       */
+/*   Updated: 2019/02/13 10:57:59 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,30 @@
 
 int			asm_match_name_or_comment(t_asm_data *data, char *line)
 {
-	/* parcour line a partir de line[data->col]
-	** qui est le premier character non ' ' ou '\t'
-	** jusqu'au premier charactere different de .name et de .comment,
-	** ou du premier ' ' ou '\t'.
-	** retourne 1 si match .name et ecrit '\0' sur le premier whitespace
-	** qui le suit.
-	** retourne 2 pour .comment et ecrit '\0' sur le premier whitespace
-	** qui le suit si il y en a un.
-	** update data->col.
-	** retourne asm_error_message(data, LEXICAL ERROR) si match pas
-	*/
-	if (data == 0 || line == 0)
-		return (-1);
-	
-	return (0);
+	int		flags;
+	int		i;
+	char	*cmd_name;
+	char	*cmd_com;
+//	char	cmd_name[ft_strlen(NAME_CMD_STRING) + 1];
+//	char	cmd_com[ft_strlen(COMMENT_CMD_STRING) + 1];
+
+	cmd_name = NAME_CMD_STRING;
+	cmd_com = COMMENT_CMD_STRING;
+	flags = 3;
+//	ft_strncpy(cmd_name, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING) + 1);
+//	ft_strncpy(cmd_com, COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING) + 1);
+	i = data->col;
+	while (flags != 0 && line[i] != ' ' && line[i] != '\t' && line[i] != '\0')
+	{
+		if (line[i] != cmd_name[i] && cmd_name[i] != '\0' && (flags & 1) == 1)
+			flags -= 1;
+		if (line[i] != cmd_com[i] && cmd_com[i] != '\0' && (flags & 2) == 1)
+			flags -= 2;
+		i++;
+	}
+	if ((flags == 0) || (line[i] != ' ' && line[i] != '\t' && line[i] != '\0')
+		|| (!(i == ft_strlen(cmd_name) || i == ft_strlen(cmd_comment))))
+		return (asm_error_msg(data, LEXICAL_ERROR));
+	data->col = i + 1;
+	return (flags);
 }
